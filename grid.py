7bf9593d7fa77043dab83a380d6dc85a8c5fa187
Anchu Lee <andrew1421lee@gmail.com>
2017-08-15 23:14:16 -0400

    new functions added

--- grid.py
+++ grid.py
@@ -3,14 +3,15 @@ import pygame
 class engine:
     WIDTH = 400 # window width
     HEIGHT = 400 # window height
-    GRIDSIZE = 40 # grid size
-    FPS = 15 # frame rate
+    GRIDSIZE = 10 # grid size
+    FPS = 30 # frame rate
     MAX_X = int(WIDTH / GRIDSIZE) # max x index
     MAX_Y = int(HEIGHT / GRIDSIZE) # max y indes
     GRIDMATRIX = [] # stores grid values
 
-    BG_COLOR = (0, 0, 0)
+    BLACK = (0, 0, 0)
     WHITE = (255, 255, 255)
+    BG_COLOR = BLACK
 
     def __init__(self):
         global clock, screen # global clock (fps) and display
@@ -23,44 +24,21 @@ class engine:
     def start(self): # start the game engine
         self.screen = pygame.display.set_mode((self.HEIGHT, self.WIDTH)) # show the screen
         self.clock = pygame.time.Clock() # set the clock
-#        i = 0
-#        done = False 
-#        while not done: # loop until exit
-#            for event in pygame.event.get():
-#                if event.type == pygame.QUIT:
-#                    done = True
-#            self.draw_rect(self.WHITE, i, i)
-#            if i % 2 == 0:
-#                self.del_rect(i - 2, i -2)
-#            i = i + 1
-#            pygame.display.flip() # update the screen
-#            self.clock.tick(self.FPS) # wait for fps clock
-
-    def get_key_press(self): # returns a key press event
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                return event.key
-
-    def check_for_exit(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return True
-        return False
 
     def refresh_screen(self):
         pygame.display.flip()
         self.clock.tick(self.FPS)
 
     def draw_rect(self, color, x, y):
-        if x >= self.MAX_X or y >= self.MAX_Y: # not allowed to go out of bounds
+        if x >= self.MAX_X or y >= self.MAX_Y or y < 0 or x < 0: # not allowed to go out of bounds
             return False
-        else: 
+        elif self.GRIDMATRIX[y][x] == 0:
             pygame.draw.rect(self.screen, color, pygame.Rect(x * self.GRIDSIZE, y * self.GRIDSIZE, self.GRIDSIZE, self.GRIDSIZE)) # draw the square
             self.GRIDMATRIX[y][x] = 1 # record its location
             return True
 
     def del_rect(self, x, y):
-        if x >= self.MAX_X or y >= self.MAX_Y:
+        if x >= self.MAX_X or y >= self.MAX_Y or y < 0 or x < 0:
             return False
         if not self.GRIDMATRIX[y][x] == 0:
             pygame.draw.rect(self.screen, self.BG_COLOR, pygame.Rect(x * self.GRIDSIZE, y * self.GRIDSIZE, self.GRIDSIZE, self.GRIDSIZE)) # draw a black square over the square
@@ -68,6 +46,10 @@ class engine:
             return True
         else:
             return False 
+
+    #def draw_mul_rects(self, rects)
+
+    #def del_mul_rects(self, rects)
 
     def print_matrix(self): # print out all contents of matrix (debug purpose)
         for xarrays in self.GRIDMATRIX:
